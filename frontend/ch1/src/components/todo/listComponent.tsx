70f06afc3cd1472f3c810d1661114c5596aa054b
eojin <ojinga32@gmail.com>
2025-11-24 23:30:18 +0900

    리액트 이론을 알아야겠다..

--- frontend/ch1/src/components/todo/listComponent.tsx
+++ frontend/ch1/src/components/todo/listComponent.tsx
@@ -1,6 +1,6 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useCustomMove from "../../hooks/useCustomMove";
-import type { useCustomMoveReturn } from "../../types/global";
+import type { PageResponseDTO, useCustomMoveReturn } from "../../types/global";
 import { getList } from "../../api/todoApi";
 
 
@@ -8,16 +8,38 @@ function ListComponent() {
     
     const {page,size,moveToRead}: useCustomMoveReturn = useCustomMove()
 
+    const [serverData, setServerData] = useState<PageResponseDTO<Todo> | undefined> ()
     useEffect(() => {
 
         getList({page, size}).then(data => {
             console.log(data)
+            setServerData(data)
         })
 
     }, [page, size])
     
     return ( 
-        <div> Todo List Component </div>
+        <div className=" border-2 border-blue-100 mt-10 mr-2 ml-2">
+            {serverData && 
+            <div className=" flex flex-wrap ms-auto justify-center p-6">
+                {serverData?.dtoList.map(todo => 
+                    <div key = {todo.tno} className=" w-full min-w-[400px] p-2 m-2 rounded shadow-md" onClick={() => moveToRead(todo.tno)}>
+                        <div className=" flex">
+                            <div className=" font-extrabold text-2xl p-2 w-1/12">
+                                {todo.tno}
+                            </div>
+                            <div className=" text-1xl m-1 p-2 w-8/12 font-extrabold">
+                                {todo.title}
+                            </div>
+                            <div className=" text-1xl m-1 p-2 w-2/10 font-medium">
+                                {todo.dueDate}
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </div>
+            }
+        </div>
      )
 }
 
